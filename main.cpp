b59e87411a889f347835afbd70e40e3fd4efaa60
ny_echo <1030620703@qq.com>
2025-04-12 15:01:56 +0800

    修复sort函数实现bug

--- main.cpp
+++ main.cpp
@@ -2,28 +2,35 @@
 #include <vector>
 using namespace std;
 
-void sort(vector<int> v,int size)
+void sort(vector<int> v, int size)
 {
-         int sum = 0;
-   for(int i = 0;i<size;++i)
-   {
-    sum +=v[i] ;
-   }
-   std::cout<<sum<<std::endl;
+    int sum = 0;
+    for (int i = 0; i < size - 1; ++i)
+    {
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (v[i] > v[j])
+            {
+                swap(v[i], v[j]);
+            }
+        }
+    }
+    std::cout << sum << std::endl;
 }
 int main()
 {
-    vector<int> v{10,9,28,30,5,7,11};
-    std::cout<<"before sort: "<<std::endl;
-    for(auto e:v)
+    vector<int> v{10, 9, 28, 30, 5, 7, 11};
+    std::cout << "before sort: " << std::endl;
+    for (auto e : v)
     {
-        std::cout<<e<<" ";
+        std::cout << e << " ";
     }
-    sort(v,v.size());
-    std::cout<<"after sort: "<<std::endl;
-    for(auto e:v)
+    sort(v, v.size());
+    std::cout << std::endl;
+    std::cout << "after sort: " << std::endl;
+    for (auto e : v)
     {
-        std::cout<<e<<" ";
+        std::cout << e << " ";
     }
     return 0;
 }
